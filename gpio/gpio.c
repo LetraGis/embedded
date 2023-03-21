4575c40ebfce83d33cac61a21b25516a13897232
LetraGis <reyes_llanos_arturo@outlook.com>
2023-03-21 15:46:14 -0400

    [GPIO] Addition of new API's for GPIO library: toggle pin, get pin state (by ref/value), pull-mode

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -6,81 +6,155 @@
  */
 
 #include "gpio.h"
+#include "stddef.h"
 
 /*!****************************************************************************
- * @details		   Initializes the indicated port.
- * @param[in]      port    Holds the port number.
- * @return         retVal
- *                 OK:     port has been enabled
- *                 N_OK:   port has not been enabled
+ * @details		   	Initializes the indicated port.
+ * @param[in]      	port    Holds the port number.
+ * @return         	retVal
+ *                 	OK:     port has been enabled
+ *                 	N_OK:   port has not been enabled due to invalid input
  ******************************************************************************/
-uint8 Gpio_Port_Init(portNumber port)
+uint8 Gpio_PortInit(portNumber port)
 {
 	uint8 retVal = N_OK;
 	/* GPIO port enable */
 	if(port < PORT_MAX)
 	{
-		*(volatile uint32 * const)(RCC_AHB1ENR_ADDRESS) |= (1u << port);
+		RCC_AHB1ENR |= (1u << port);
 		retVal = OK;
-	}
-	else
-	{
-		retVal = N_OK;
 	}
 	return retVal;
 }
 
 /*!****************************************************************************
- * @details		   Sets the port mode as output, input,
- * @param[in]      port    Holds the port number.
- * @return         retVal
- *                 OK:     request successful
- *                 N_OK:   Request was not successful
+ * @details		   	Sets the port mode as output, input, alternate or analog.
+ * @param[in]      	port    Holds the port number.
+ * @param[in]      	pin     Holds the pin number.
+ * @param[in]      	mode    Holds the mode: out/in/alternate/analog
+ * @return         	retVal
+ *                 	OK:     request successful
+ *                 	N_OK:   Request was not successful
  ******************************************************************************/
-uint8 Gpio_Pin_Mode(portNumber port, uint8 pin, portMode mode)
+uint8 Gpio_PinMode(portNumber port, pinNumber pin, portMode mode)
 {
 	uint8 retVal = N_OK;
 	if((port < PORT_MAX) && (mode < PIN_MODE_MAX ) && (pin < PIN_NUM_MAX))
 	{
-		*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u))
-				&= (~(uint32)(1u << (pin * 2u)) | ~(uint32)(1u << ((pin * 2u) + 1u)));
-		*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u)) |= ((uint32)(mode << (pin * 2)));
+		/* Clear bits before setting them to mode */
+		GPIOX_MODER(port) &= (~(uint32)(1u << (pin * 2u)) | ~(uint32)(1u << ((pin * 2u) + 1u)));
+		/* Setting pin to mode */
+		GPIOX_MODER(port) |= ((uint32)(mode << (pin * 2)));
 		retVal = OK;
-	}
-	else
-	{
-		retVal = N_OK;
 	}
 	return retVal;
 }
 
 /*!****************************************************************************
- * @details		   Initializes the indicated port.
- * @param[in]      port    Holds the port number.
- * @return         retVal
- *                 OK:     request successful
- *                 N_OK:   Request was not successful
+ * @details		   	Sets the given pin of the given port to the state given.
+ * @param[in]      	port    Holds the port number.
+ * @param[in]      	pin	    Holds the pin number.
+ * @param[in]      	state   Holds the state to be set on the pin: HIGH/LOW
+ * @return         	retVal
+ *                 	OK:     request successful
+ *                 	N_OK:   Request was not successful
  ******************************************************************************/
-uint8 Gpio_Pin_State(portNumber port, uint8 pin, pinState state)
+uint8 Gpio_SetPinState(portNumber port, pinNumber pin, pinState state)
 {
 	uint8 retVal = N_OK;
 	if((port < PORT_MAX) && (pin < PIN_NUM_MAX))
 	{
-		if(state == low)
+		if(state == high)
 		{
-			*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u) + 0x14u) |= (uint32)(1u << pin);
+			GPIOX_ODR(port) |= (uint32)(1u << pin);
 			retVal = OK;
 		}
-		else if(state == high)
+		else if(state == low)
 		{
-			*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u) + 0x14u) &= ~(uint32)(1u << pin);
+			GPIOX_ODR(port) &= ~(uint32)(1u << pin);
 			retVal = OK;
-		}
-		else
-		{
-			retVal = N_OK;
 		}
 	}
 	return retVal;
 }
 
+/*!****************************************************************************
+ * @details		   	Initializes the indicated port.
+ * @param[in]      	port    Holds the port number.
+ * @param[in]      	pin     Holds the pin number.
+ * @return         	retVal
+ *                 	OK:     request successful
+ *                 	N_OK:   Request was not successful
+ ******************************************************************************/
+uint8 Gpio_TogglePinState(portNumber port, pinNumber pin)
+{
+	uint8 retVal = N_OK;
+	if((port < PORT_MAX) && (pin < PIN_NUM_MAX))
+	{
+		GPIOX_ODR(port) ^= (uint32)(1u << pin);
+		retVal = OK;
+	}
+	return retVal;
+}
+
+/*!****************************************************************************
+ * @details			Gets the status of the given pin of the given port.
+ * @param[in]      	port    Holds the port number.
+ * @param[in]      	pin     Holds the pin number.
+ * @param[out]     	state   Returns the state of the given pin.
+ * @return         	retVal
+ *                 	OK:     request successful
+ *                 	N_OK:   Request was not successful
+ ******************************************************************************/
+uint8 Gpio_GetPinStateRef(portNumber port, pinNumber pin, pinState *state)
+{
+	uint8 retVal = N_OK;
+	if((port < PORT_MAX) && (pin < PIN_NUM_MAX) && (state != NULL))
+	{
+		*state = ((GPIOX_IDR(port))>>pin)&1u;
+		retVal = OK;
+	}
+	return retVal;
+}
+
+/*!****************************************************************************
+ * @details			Gets the status of the given pin of the given port.
+ * @param[in]      	port    Holds the port number.
+ * @param[in]      	pin     Holds the pin number.
+ * @param[out]     	state   Returns the state of the given pin.
+ * @return         	retVal
+ *                 	OK:     request successful
+ *                 	N_OK:   Request was not successful
+ ******************************************************************************/
+uint8 Gpio_GetPinStateVal(portNumber port, pinNumber pin)
+{
+	pinState retVal = RET_ERROR;
+	if((port < PORT_MAX) && (pin < PIN_NUM_MAX))
+	{
+		retVal = ((GPIOX_IDR(port))>>pin)&1u;
+	}
+	return retVal;
+}
+
+/*!****************************************************************************
+ * @details		   	Sets the port mode as output, input, alternate or analog.
+ * @param[in]      	port    Holds the port number.
+ * @param[in]      	pin     Holds the pin number.
+ * @param[in]      	mode    Holds the mode: out/in/alternate/analog
+ * @return         	retVal
+ *                 	OK:     request successful
+ *                 	N_OK:   Request was not successful
+ ******************************************************************************/
+uint8 Gpio_PullMode(portNumber port, pinNumber pin, pullMode mode)
+{
+	uint8 retVal = N_OK;
+	if((port < PORT_MAX) && (mode < PIN_MODE_MAX ) && (pin < PIN_NUM_MAX))
+	{
+		/* Clear bits before setting them to mode */
+		GPIOX_PUPDR(port) &= (~(uint32)(1u << (pin * 2u)) | ~(uint32)(1u << ((pin * 2u) + 1u)));
+		/* Setting pin to mode */
+		GPIOX_PUPDR(port) |= ((uint32)(mode << (pin * 2)));
+		retVal = OK;
+	}
+	return retVal;
+}
--- gpio/gpio.h
+++ gpio/gpio.h
@@ -11,6 +11,7 @@
 #include <stdint.h>
 
 typedef uint32_t uint32;
+typedef uint16_t uint16;
 typedef uint8_t uint8;
 
 /* RCC register to enable GPIO ports */
@@ -18,13 +19,26 @@ typedef uint8_t uint8;
 /* GPIO base register address */
 #define GPIOX_REG_BASE_ADDRESS 0x40020000u
 
+#define RCC_AHB1ENR (*(volatile uint32 * const)(RCC_AHB1ENR_ADDRESS))
+#define GPIOX_MODER(port) (*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u)))
+#define GPIOX_PUPDR(port) (*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u) + 0x0Cu))
+#define GPIOX_IDR(port) (*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u) + 0x10u))
+#define GPIOX_ODR(port) (*(volatile uint32 * const)(GPIOX_REG_BASE_ADDRESS + (port * 0x400u) + 0x14u))
+
 #define HIGH (1u)
 #define LOW (0)
 #define OK (0u)
 #define N_OK (1u)
-#define PORT_MAX (8u) //Maximum number of ports
-#define PIN_MODE_MAX (4u) //Maximum number of pin modes
-#define PIN_NUM_MAX (16u) //Maximum number of pins per port
+#define RET_ERROR (255u)
+
+/* Maximum number of ports */
+#define PORT_MAX (8u)
+/* Maximum number of pin modes */
+#define PIN_MODE_MAX (4u)
+/* Maximum number of pins per port */
+#define PIN_NUM_MAX (16u)
+/* Maximum number of pull up/down configurations */
+#define PULL_MODE_MAX (3u)
 
 typedef enum
 {
@@ -46,6 +60,33 @@ typedef enum
 
 typedef enum
 {
+	pin0 = 0,
+	pin1,
+	pin2,
+	pin3,
+	pin4,
+	pin5,
+	pin6,
+	pin7,
+	pin8,
+	pin9,
+	pin10,
+	pin11,
+	pin12,
+	pin13,
+	pin14,
+	pin15,
+} pinNumber;
+
+typedef enum
+{
+	none = 0,
+	pullup,
+	pulldown,
+} pullMode;
+
+typedef enum
+{
 	input = 0,
 	output,
 	alternate,
@@ -53,8 +94,12 @@ typedef enum
 } portMode;
 
 /* Function prototypes */
-uint8 Gpio_Port_Init(portNumber port);
-uint8 Gpio_Pin_Mode(portNumber port, uint8 pin, portMode mode);
-uint8 Gpio_Pin_State(portNumber port, uint8 pin, pinState state);
+uint8 Gpio_PortInit(portNumber port);
+uint8 Gpio_PinMode(portNumber port, pinNumber pin, portMode mode);
+uint8 Gpio_SetPinState(portNumber port, pinNumber pin, pinState state);
+uint8 Gpio_TogglePinState(portNumber port, pinNumber pin);
+uint8 Gpio_GetPinStateRef(portNumber port, pinNumber pin, pinState *state);
+uint8 Gpio_GetPinStateVal(portNumber port, pinNumber pin);
+uint8 Gpio_PullMode(portNumber port, pinNumber pin, pullMode mode);
 
 #endif /* GPIO_H_ */
