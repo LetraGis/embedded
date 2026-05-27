caf2a2199e0a0ef040edd4b794521267c7eaeb40
LetraGis <reyes_llanos_arturo@outlook.com>
2026-05-26 22:58:46 -0400

    [Build system] Adding the first set of files for toolchain support

---
+++ src/bsp.c
@@ -0,0 +1,205 @@
+/*
+ * bsp.c
+ *
+ *  Created on: Nov 2, 2022
+ *      Author: LARL
+ */
+
+/******************************************************************************
+EXTERNAL DEPENDENCIES
+******************************************************************************/
+
+#include "bsp.h"
+
+/******************************************************************************
+MODULE DEFINES
+******************************************************************************/
+
+/******************************************************************************
+MODULE TYPES
+******************************************************************************/
+
+/******************************************************************************
+DECLARATION OF LOCAL FUNCTIONS
+******************************************************************************/
+
+/******************************************************************************
+DEFINITION OF LOCAL VARIABLES
+******************************************************************************/
+
+/******************************************************************************
+DEFINITION OF EXPORTED VARIABLES
+******************************************************************************/
+
+/******************************************************************************
+DEFINITION OF LOCAL CONSTANT DATA
+******************************************************************************/
+
+/******************************************************************************
+DEFINITION OF EXPORTED CONSTANT DATA
+******************************************************************************/
+
+/******************************************************************************
+MODULE FUNCTION-LIKE MACROS
+******************************************************************************/
+
+/******************************************************************************
+DEFINITION OF APIs
+******************************************************************************/
+
+/*!****************************************************************************
+ * @brief			Initializes RTOS kernel.
+ * @details		   	Initializes RTOS by setting the priority of PendSV
+ *                  exception to the lowest (which is used for context
+ *                  switching) and starts the idle thread.
+ * @param[in]      	void    No input parameters.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void SysTick_Handler(void) 
+{
+
+	OS_Tick();
+
+    __disable_irq();
+    OS_Sched();
+    __enable_irq();
+
+}
+
+/*!****************************************************************************
+ * @brief			Initializes RTOS kernel.
+ * @details		   	Initializes RTOS by setting the priority of PendSV
+ *                  exception to the lowest (which is used for context
+ *                  switching) and starts the idle thread.
+ * @param[in]      	void    No input parameters.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void OS_onStartup_Callback(void)
+{
+    /* Function given by core M4 CMSIS. We provide number of ticks as input. */
+	(void)SysTick_Config(BSP_SYS_CLOCK_HZ/BSP_TICKS_PER_SEC);
+
+	NVIC_SetPriority(SysTick_IRQn, 0U);
+
+	/* Alternatively we can set the fields individually using CMSIS
+	structure as shown bellow. */
+	/* Specifies the start value of SysTick Counter */
+	/* SysTick->LOAD = SYS_CLOCK_HZ - 1U; */
+    /* Specifies the current value of SysTick Counter */
+	/* SysTick->VAL  = 0U; */
+    /* Bit 2 - ClkSource (Internal OScillator - HSI in this case) */
+	/* Bit 1 - TickInt (Enables exception request when counting down to 0) */
+	/* Bit 0 - Enables SysTick Counter (Counter reloads value from LOAD reg) */
+	/* SysTick->CTRL = (1U << 2) | (1U << 1) | (1U << 0); */
+}
+
+/*!****************************************************************************
+ * @brief			Initializes RTOS kernel.
+ * @details		   	Initializes RTOS by setting the priority of PendSV
+ *                  exception to the lowest (which is used for context
+ *                  switching) and starts the idle thread.
+ * @param[in]      	void    No input parameters.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void OS_onIdle_Callback(void)
+{
+    __WFI();
+}
+
+/*!****************************************************************************
+ * @brief			Initializes RTOS kernel.
+ * @details		   	Initializes RTOS by setting the priority of PendSV
+ *                  exception to the lowest (which is used for context
+ *                  switching) and starts the idle thread.
+ * @param[in]      	void    No input parameters.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void Gpio_InitCallback(void)
+{
+	/* Pin 13 of Port C is connected to On-Board User Button */
+	(void)Gpio_PortInit(portc);
+	(void)Gpio_PinMode(portc, pin13, input);
+
+	/* SPI Related Configuration */
+	Gpio_PortInit(porta);
+	Gpio_PinMode(porta, pin4, alternate);
+	Gpio_PinMode(porta, pin5, alternate);
+	Gpio_PinMode(porta, pin6, alternate);
+	Gpio_PinMode(porta, pin7, alternate);
+
+	Gpio_ConfigAltFnc(porta, pin4, altFun5);
+	Gpio_ConfigAltFnc(porta, pin5, altFun5);
+	Gpio_ConfigAltFnc(porta, pin6, altFun5);
+	Gpio_ConfigAltFnc(porta, pin7, altFun5);
+
+	Gpio_OutputTypeCfg(porta, pin4, pushPull);
+	Gpio_OutputTypeCfg(porta, pin5, pushPull);
+	Gpio_OutputTypeCfg(porta, pin6, pushPull);
+	Gpio_OutputTypeCfg(porta, pin7, pushPull);
+
+	Gpio_PullMode(porta, pin4, none);
+	Gpio_PullMode(porta, pin5, none);
+	Gpio_PullMode(porta, pin6, none);
+	Gpio_PullMode(porta, pin7, none);
+
+	Gpio_OutputSpeed(porta, pin4, highSpeed);
+	Gpio_OutputSpeed(porta, pin5, highSpeed);
+	Gpio_OutputSpeed(porta, pin6, highSpeed);
+	Gpio_OutputSpeed(porta, pin7, highSpeed);
+}
+
+/*!****************************************************************************
+ * @brief			Initializes SPI.
+ * @details		   	Initializes SPI. Implementation is up to the developer. For
+ * 					the example, SPI1 Configuration Register 1 is configured,
+ * 					and SPI1 Peripheral 1 is enabled.
+ * @param[in]      	void    No input parameters.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void Spi_InitCallback(void)
+{	
+	SPI1_ENABLE();
+	Spi_Cfg1RegType spi1cfg1;
+	spi1cfg1.Bytes = 0;
+	spi1cfg1.Fields.BIDIMODE = twoLineUni;
+	spi1cfg1.Fields.BR = pclkDivBy256;
+	spi1cfg1.Fields.CPHA = firstEdge;
+	spi1cfg1.Fields.CPOL = clkPolLowIdle;
+	spi1cfg1.Fields.DFF = oneByte;
+	spi1cfg1.Fields.MSTR = spiMaster;
+	spi1cfg1.Fields.SSM = swMgtDisabled;
+
+	Spi_Config_CR1(SPI_1, spi1cfg1.Bytes);
+}
+
+/*!****************************************************************************
+ * @brief			Initializes RTOS kernel.
+ * @details		   	Initializes RTOS by setting the priority of PendSV
+ *                  exception to the lowest (which is used for context
+ *                  switching) and starts the idle thread.
+ * @param[in]      	void    No input parameters.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void Q_onAssert(char const *module, int loc)
+{
+	/* TBD */
+	(void)module;
+	(void)loc;
+	NVIC_SystemReset();
+}
+
+/*!****************************************************************************
+ * @brief			Delays execution code.
+ * @details		   	Waits for the number of ticks given by the input.
+ * @param[in]      	ticks   Number of CPU cycles wasted.
+ * @return         	void    No output parameters.
+ ******************************************************************************/
+void delay(const uint32_t ticks)
+{
+	volatile uint32_t i;
+	for (i = 0; i < ticks; i++);
+}
+
+/******************************************************************************
+End Of File
+******************************************************************************/
---
+++ toolchain/cc_toolchain_config.bzl
@@ -0,0 +1,84 @@
+load("@rules_cc//cc:action_names.bzl", "ACTION_NAMES")
+load("@rules_cc//cc/common:cc_common.bzl", "cc_common")
+load("@rules_cc//cc:cc_toolchain_config_lib.bzl",  "feature", "flag_set", "flag_group")
+
+def _impl(ctx):
+    # Define the base path for compiler utilities
+    compiler_bin_path = "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10/bin/arm-none-eabi-"
+    # Define the path to the include directory
+    compiler_include_path = "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10/arm-none-eabi/include"
+    
+    # Specify the exact path to your local toolchain binaries
+    tool_paths = [
+        struct(name = "gcc", path = compiler_bin_path + "gcc"),
+        struct(name = "cpp", path = compiler_bin_path + "g++"),
+        struct(name = "ld", path = compiler_bin_path + "ld"),
+        # struct(name = "ld", path = "arm-none-eabi-gcc"), # Using gcc wrapper to link is best practice
+        struct(name = "ar", path = compiler_bin_path + "ar"),
+        struct(name = "strip", path = compiler_bin_path + "strip"),
+        struct(name = "objcopy", path = compiler_bin_path + "objcopy"),
+        struct(name = "objdump", path = compiler_bin_path + "objdump"),
+    ]
+
+    # Combine all actions for compilation and linking
+    all_compile_actions = [
+        ACTION_NAMES.c_compile,
+        ACTION_NAMES.cpp_compile,
+        ACTION_NAMES.assemble,
+        ACTION_NAMES.preprocess_assemble,
+    ]
+    all_link_actions = [
+        ACTION_NAMES.cpp_link_executable,
+    ]
+
+    # 2. Package global hardware specs as a Bazel Feature
+    hardware_flags_feature = feature(
+        name = "hardware_flags",
+        enabled = True,
+        flag_sets = [
+            flag_set(
+                actions = all_compile_actions + all_link_actions,
+                flag_groups = [
+                    flag_group(
+                        flags = [
+                            "-mcpu=cortex-m4",
+                            "-mfpu=fpv4-sp-d16",
+                            "-mfloat-abi=soft",  # Using the soft ABI flag from your IDE specs
+                            "-mthumb",
+                            "--specs=nano.specs",
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    features = [hardware_flags_feature]
+
+    return cc_common.create_cc_toolchain_config_info(
+        ctx = ctx,
+        features = features,
+        action_configs = [],
+        artifact_name_patterns = [],
+        cxx_builtin_include_directories = [
+            # Tells Bazel where it's safe to look for compiler-provided headers.
+            # If your toolchain throws missing standard header errors later,
+            # we will explicitly add your system toolchain include paths here.
+            compiler_include_path,
+        ],
+        toolchain_identifier = "local_arm_none_eabi",
+        host_system_name = "local",
+        target_system_name = "arm-none-eabi",
+        target_cpu = "cortex-m4",
+        target_libc = "unknown",
+        compiler = "arm-none-eabi-gcc",
+        abi_version = "unknown",
+        abi_libc_version = "unknown",
+        tool_paths = tool_paths,
+    )
+
+cc_toolchain_config = rule(
+    implementation = _impl,
+    attrs = {},
+    provides = [cc_common.CcToolchainInfo],
+)
